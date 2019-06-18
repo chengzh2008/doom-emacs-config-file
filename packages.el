844301340efedf18c7b6a195585eafeac5bfdc16
chengzh2008 <chengzh2008@gmail.com>
2019-06-18 10:44:52 -0700

    update doom emacs config

---
+++ packages.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; -*-
+;;; ~/.doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+(package! doom-themes)
+(package! prettier-js)
+;; (package! typescript-mode)
+(package! tide)
+;;(package! default-text-scale-mode)
